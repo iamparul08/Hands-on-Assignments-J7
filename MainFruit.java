24a5c2f84f2c7a09cf1c8bda633ce4c2fb1efabb
Parul Mathur <46438911+iamparul08@users.noreply.github.com>
2020-05-09 11:37:58 +0530

    Add files via upload

---
+++ MainFruit.java
@@ -0,0 +1,61 @@
+/* Create  a base class Fruit with name ,taste and size as its attributes. 
+Create a method called eat() which describes the name of the fruit and its taste. 
+Inherit the same in 2 other classes Apple and Orange and override the eat() method to represent each fruit taste.*/
+
+import java.util.*;
+
+class Fruit {
+	
+	String name, taste, size;
+	
+	public void eat() {	//overridden method
+		
+		Scanner s1 = new Scanner(System.in);
+		
+		System.out.println("Enter the name of the Fruit- "); 
+		name = s1.nextLine();
+		
+		System.out.println("Enter the taste of the fruit- ");
+		taste = s1.nextLine();
+		
+		System.out.println("Enter the size of the fruit- ");
+		size = s1.nextLine();
+		
+		
+		}
+		
+}
+
+class Apple extends Fruit {	
+	
+	public void eat() { 		//overriding method
+		
+		System.out.println("Name of the fruit is Apple.");
+		System.out.println("Taste of an apple is sweet.");
+	
+	}
+	
+}
+
+class Orange extends Fruit {	
+	
+	public void eat() { 		//overriding method
+	
+		System.out.println("Name of fruit is Orange.");
+		System.out.println("Taste of orange is sour.");
+		
+	}
+	
+}
+
+class MainFruit {
+	
+	public static void main(String args[]) {
+		
+		Apple a = new Apple();
+		a.eat();
+		
+		Orange o = new Orange();
+		o.eat();
+	}
+}
